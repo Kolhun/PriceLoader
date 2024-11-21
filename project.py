1b088f384e4564aecf43c58cdd6873f72d10abdc
Kolhun <36815965+Kolhun@users.noreply.github.com>
2024-11-21 13:53:17 +0300

    Second commit

---
+++ project.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+from prettytable import PrettyTable
+
+class PriceAnalyzer:
+    def __init__(self, folder):
+        self.folder = folder
+        self.data = pd.DataFrame()
+    def load_prices (self):
+        pass
+    def find_text  (self):
+        pass
+    def export_to_html  (self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
